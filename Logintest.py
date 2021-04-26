a7f745524fa71a2e92cf30b56f169d81a2610313
MiroTW <43741555+MiroTW@users.noreply.github.com>
2021-04-26 17:24:46 +0800

    Update Logintest.py

--- Logintest.py
+++ Logintest.py
@@ -16,7 +16,7 @@ class OHF(unittest.TestCase):
 #         opts.add_argument("--no-sandbox")
 #         opts.add_argument("--disable-dev-shm-usage")
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=opts)
-        self.driver.get('https://www.104.com.tw/jobs/main/')
+        self.driver.get('https://signin.104.com.tw/')
 #         self.driver.maximize_window()
 
     def tearDown(self):
@@ -27,7 +27,7 @@ class OHF(unittest.TestCase):
         time.sleep(10)
 #         self.driver.find_element_by_xpath('/html/body/div[1]/div/div/ul/li[2]/ul/li[6]/a').click()
 #         self.driver.find_element_by_xpath('//*[@id="global_bk"]/ul/li[2]/ul/li[6]/a').click()
-        self.driver.find_element_by_xpath('//*[@class="right"]/ul/li[6]/a').click()
+#         self.driver.find_element_by_xpath('//*[@class="right"]/ul/li[6]/a').click()
         self.driver.find_element_by_id('username').send_keys(os.environ['USERNAME'])
         self.driver.find_element_by_id('password').send_keys(os.environ['PASSWORD'])
         self.driver.find_element_by_id('submitBtn').click()
