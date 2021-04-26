70edb3de114a18cdeb8f1854bbaaa256ae38ab4e
MiroTW <43741555+MiroTW@users.noreply.github.com>
2021-04-26 16:42:05 +0800

    Update Logintest.py

--- Logintest.py
+++ Logintest.py
@@ -8,9 +8,9 @@ import os
 
 class OHF(unittest.TestCase):
     def setUp(self):
-        warnings.simplefilter('ignore', ResourceWarning)
+#         warnings.simplefilter('ignore', ResourceWarning)
         opts = Options()
-        opts.add_argument("--no-sandbox")
+#         opts.add_argument("--no-sandbox")
         opts.add_argument('--headless')
         opts.add_argument('--disable-gpu')
 #         opts.add_argument("--no-sandbox")
